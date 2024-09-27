a984b04e94fc028ef47210fd5b25a3c291244c6e
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2024-09-27 02:03:34 +0200

    move secrets to .env. they got leaked and should be chnaged.

--- backend/express-server/db-object.js
+++ backend/express-server/db-object.js
@@ -1,11 +1,12 @@
 const pgp = require("pg-promise")(/* options */);
+require('dotenv').config()
 const cn = {
-  host: "aws-0-eu-central-2.pooler.supabase.com",
-  port: 6543,
-  database: "postgres",
-  user: "postgres.xfutqhawurxfjauhpybh",
-  password: "cBp8vaVFJ6ST14UM",
-  max: 30, // use up to 30 connections
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  max: process.env.DB_MAX, // use up to 30 connections
   // "types" - in case you want to set custom type parsers on the pool level
 };
 
