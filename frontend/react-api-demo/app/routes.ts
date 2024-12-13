cc6b8d6c6717f65615d345814cc2d6a541c7536b
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2024-12-13 11:54:43 +0100

    Remove react-api-demo

--- frontend/react-api-demo/app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/Home.tsx"),
-  route("signup", "routes/Signup.tsx"),
-  route("login", "routes/Login.tsx"),
-] satisfies RouteConfig;
